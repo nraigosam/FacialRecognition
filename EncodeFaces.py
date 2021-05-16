8149efd0a33e51b355f5d0dcf7b1760b01bc30f2
Nicolas Raigosa <nraigosam@eafit.edu.co>
2021-05-15 20:17:19 -0500

    Codigo base de reconocimiento facial

---
+++ EncodeFaces.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+import pickle
+# from PIL import ImageGrab
+ 
+path = 'dataset'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    with open('dataset_faces.dat', 'wb') as f:
+        pickle.dump(encodeList, f)
+
+
+findEncodings(images)
+
