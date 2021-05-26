deadb7e390bd2f39b0a5478ed0b9b75cdf54bde9
Nicolas Raigosa <nraigosam@eafit.edu.co>
2021-05-26 12:34:01 -0500

    organizacion d earchivos

--- FaceDetection.py
+++ FaceDetection.py
@@ -6,68 +6,70 @@ from datetime import datetime, time
 import pickle
 # from PIL import ImageGrab
  
-path = 'dataset'
-face = 0
-images = []
-classNames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
-    classNames.append(os.path.splitext(cl)[0])
+class FaceDetection:
+    def __init__(self):
+        classNames = self.loadNames()
+        encodeListKnown = self.encode()
+        self.detection(encodeListKnown, classNames)
 
- 
-# def markAttendance(name, faceDis):
-#     with open('Attendance.csv','r+') as f:
-#         myDataList = f.readlines()
-#         for line in myDataList:
-#             face = faceDis
-#             now = datetime.now()
-#             dtString = now.strftime('%H:%M:%S')
-#             f.writelines(f'\n{faceDis}')
-#             # f.writelines(f'\n{name},{faceDis},{dtString}')
+    def loadNames(self):
+        path = 'dataset'
+        images = []
+        classNames = []
+        myList = os.listdir(path)
 
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
+        for cl in myList:
+            curImg = cv2.imread(f'{path}/{cl}')
+            images.append(curImg)
+            classNames.append(os.path.splitext(cl)[0])
 
-with open('dataset_faces.dat', 'rb') as f:
-	encodeListKnown = pickle.load(f)
+        return classNames
 
-print(encodeListKnown)
-print('Encoding Complete')
- 
-cap = cv2.VideoCapture(0)
- 
-while True:
+    #### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+    # def captureScreen(bbox=(300,300,690+300,530+300)):
+    #     capScr = np.array(ImageGrab.grab(bbox))
+    #     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+    #     return capScr
 
-    success, img = cap.read()
-    #img = captureScreen()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
- 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    def encode(self):
+        with open('dataset_faces.dat', 'rb') as f:
+            encodeListKnown = pickle.load(f)
+        return encodeListKnown
+
     
-    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        matchIndex = np.argmin(faceDis)
+    def detection(self, encodeListKnown, classNames):
+        cap = cv2.VideoCapture(0)
     
-        if faceDis[matchIndex]< 0.50:
+        while True:
+
+            _, img = cap.read()
+            # img = face_recognition.load_image_file('imagen.jpg')
+            # img = captureScreen()
+            imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        
+            facesCurFrame = face_recognition.face_locations(imgS)
+            encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
             
-            name = classNames[matchIndex].upper()
-            name = name.split('_')[0]
+            for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+                matchIndex = np.argmin(faceDis)
+            
+                if faceDis[matchIndex]< 0.50:
+                    
+                    name = classNames[matchIndex].upper()
+                    name = name.split('_')[0]
 
-        else: name = 'Unknown'
-        y1,x2,y2,x1 = faceLoc
-        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
- 
-    cv2.imshow('Webcam',img)
-    cv2.waitKey(1)
+                else: name = 'Unknown'
+                y1,x2,y2,x1 = faceLoc
+                y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+                cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+                cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        
+            cv2.imshow('Webcam',img)
+            cv2.waitKey(1)
+
+
+faceDetection = FaceDetection()
